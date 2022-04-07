dbf70bba22dea536b50ca957ff48aaf3f13b7a75
Arshad <mohammedarshad183@gmail.com>
2022-04-07 21:11:01 +0530

    Refactor:Store operation in dictionary

--- Computation.sh
+++ Computation.sh
@@ -1,16 +1,14 @@
 #!/bin/bash -x
+declare -A dict;
 read -p "Enter the value of a : " a
 read -p "Enter the value of b : " b
 read -p "Enter the value of c : " c
 
-compute1=$(($a+$b*$c));
-compute2=$(($a*$b+$c));
-compute3=$(($c+$a/$b));
-compute4=$(($a%$b+$c));
-echo $compute1;
-echo $compute2;
-echo $compute3;
-echo $compute4;
+dict[compute1]=$(($a+$b*$c));
+dict[compute2]=$(($a*$b+$c));
+dict[compute3]=$(($c+$a/$b));
+dict[compute4]=$(($a%$b+$c));
+echo ${dict[@]};
 
 
 
