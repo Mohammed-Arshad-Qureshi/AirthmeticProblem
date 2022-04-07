709b1d23db1f5706472bbb2b0129b52a597b9cfd
Arshad <mohammedarshad183@gmail.com>
2022-04-07 20:56:26 +0530

    Refactor:adding operation a*b+c

--- Computation.sh
+++ Computation.sh
@@ -3,9 +3,10 @@ read -p "Enter the value of a : " a
 read -p "Enter the value of b : " b
 read -p "Enter the value of c : " c
 
-compute=$(($a+$b*$c));
-echo $compute;
-
+compute1=$(($a+$b*$c));
+compute2=$(($a*$b+$c));
+echo $compute1;
+echo $compute2;
 
 
 
