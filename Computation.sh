c0bcc8deb8fa383cf3c6818918ea89be621f0f8f
Arshad <mohammedarshad183@gmail.com>
2022-04-07 21:27:50 +0530

    Refactor:Adding array

--- Computation.sh
+++ Computation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 declare -A dict;
+declare -a arr;
+count=0
 read -p "Enter the value of a : " a
 read -p "Enter the value of b : " b
 read -p "Enter the value of c : " c
@@ -8,7 +10,13 @@ dict[compute1]=$(($a+$b*$c));
 dict[compute2]=$(($a*$b+$c));
 dict[compute3]=$(($c+$a/$b));
 dict[compute4]=$(($a%$b+$c));
-echo ${dict[@]};
+for i in ${dict[@]}
+do
+	arr[count]=$i;
+	((count++));
+done
+echo ${arr[@]};
+
 
 
 
