3a48e2709a0be93a76bf2c8e4017b4f43ebe6b92
Arshad <mohammedarshad183@gmail.com>
2022-04-08 12:21:58 +0530

    Refactor: done sorting on arrays

--- Computation.sh
+++ Computation.sh
@@ -14,36 +14,36 @@ dict[compute3]=$(($c+$a/$b));
 dict[compute4]=$(($a%$b+$c));
 for data in ${dict[@]}
 do
-	arr[count]=$data;
-	((count++));
+        arr[count]=$data;
+        ((count++));
 done
 echo -n "Before sorting the elements : "
 echo ${arr[@]};
 for ((i=0; i<${#arr[@]}; i++))
 do
-	for ((j=$i+1; j<${#arr[@]}; j++))
-	do
-		if [ ${arr[$i]} -lt ${arr[$j]} ]
-		then
-			temp1=${arr[$j]};
-			arr[$j]=${arr[$i]};
-			arr[$i]=$temp1;
-		fi
-	done
+        for ((j=$i+1; j<${#arr[@]}; j++))
+        do
+                if [ ${arr[$i]} -lt ${arr[$j]} ]
+                then
+                        temp1=${arr[$j]};
+                        arr[$j]=${arr[$i]};
+                        arr[$i]=$temp1;
+                fi
+        done
 done
 echo -n "After sorting the elements in Descending order : "
 echo ${arr[@]};
 for ((i=0; i<${#arr[@]}; i++))
 do
-	for ((j=$i+1; j<${#arr[@]};j++))
-	do
-		if [ ${arr[$i]} -gt ${arr[$j]} ]
-		then
-			temp2=${arr[$i]};
-			arr[$i]=${arr[$j]};
-			arr[$j]=$temp2;
-		fi
-	done
+        for ((j=$i+1; j<${#arr[@]};j++))
+        do
+                if [ ${arr[$i]} -gt ${arr[$j]} ]
+                then
+                        temp2=${arr[$i]};
+                        arr[$i]=${arr[$j]};
+                        arr[$j]=$temp2;
+                fi
+        done
 done
 echo -n "After sorting the elements in ascending order : ";
 echo ${arr[@]};
